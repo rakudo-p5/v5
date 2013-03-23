166db91f9c1a22f66b5c2f80cbf7545b9e85be54
Pawel Murias <pawelmurias@gmail.com>
2013-03-23 19:42:27 +0100

    Document some hacks.

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ v5
 
 v5.pm for rakudo
 
+Currently v5.pm is compiled into perl5.pbc as 'use v5' is currently a noop in rakudo so temporarily use 'use perl5'.
+
 make
 PERL6LIB=blib perl6 t/v5/basic.t
--- lib/v5.pm
+++ lib/v5.pm
@@ -1,10 +1,14 @@
 use Perl6::Grammar;
 use Perl6::P5Grammar;
+
+# we use the MOP because that's the only way nqp supports multiple inheritance 
 grammar Foo {
 }
 my $grammar := Foo.HOW.new_type();
 $grammar.HOW.add_parent($grammar,Perl6::P5Grammar);
+# we need to add that so that the rebless of the cursor works, we don't use it otherwise
 $grammar.HOW.add_parent($grammar,Perl6::Grammar);
 $grammar.HOW.compose($grammar);
+
 %*LANG<MAIN> := $grammar;
 %*LANG<MAIN-actions> := Perl6::P5Actions;
