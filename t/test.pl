fa1ff4aef86b56279af2998eb76fb750f28d3178
Tobias Leich <email@froggs.de>
2013-05-13 00:13:00 +0200

    added all test subs from p6-Test.pm

--- t/test.pl
+++ t/test.pl
@@ -1,10 +1,26 @@
 
 use 6.0.0;
 use Test;
-sub _plan(*@args) { plan(|@args) }
-sub _is(*@args)   { is(|@args)   }
-sub _ok(*@args)   { ok(|@args)   }
-sub _skip(*@args) { skip(|@args) }
+sub _plan(*@args)          { plan(|@args)          }
+sub _pass(*@args)          { pass(|@args)          }
+sub _ok(*@args)            { ok(|@args)            }
+sub _nok(*@args)           { nok(|@args)           }
+sub _is(*@args)            { is(|@args)            }
+sub _isnt(*@args)          { isnt(|@args)          }
+sub _is_approx(*@args)     { is_approx(|@args)     }
+sub _todo(*@args)          { todo(|@args)          }
+sub _skip(*@args)          { skip(|@args)          }
+sub _skip_rest(*@args)     { skip_rest(|@args)     }
+sub _diag(*@args)          { diag(|@args)          }
+sub _flunk(*@args)         { flunk(|@args)         }
+sub _isa_ok(*@args)        { isa_ok(|@args)        }
+sub _dies_ok(*@args)       { dies_ok(|@args)       }
+sub _lives_ok(*@args)      { lives_ok(|@args)      }
+sub _eval_dies_ok(*@args)  { eval_dies_ok(|@args)  }
+sub _eval_lives_ok(*@args) { eval_lives_ok(|@args) }
+sub _is_deeply(*@args)     { is_deeply(|@args)     }
+sub _done_testing()        { done_testing()        }
+sub _done()                { done()                }
 
 {
     use v5;
@@ -12,7 +28,24 @@ sub _skip(*@args) { skip(|@args) }
         my %args = scalar(@_) == 2 ? @_ : tests => $_[0];
         _plan( $args{'tests'} )
     }
-    sub is   { _is( @_ )   }
-    sub ok   { _ok( @_ )   }
-    sub skip { _skip( @_ ) }
+    sub pass          { _pass(@_)          }
+    sub ok            { _ok(@_)            }
+    sub nok           { _nok(@_)           }
+    sub is            { _is(@_)            }
+    sub isnt          { _isnt(@_)          }
+    sub is_approx     { _is_approx(@_)     }
+    sub todo          { _todo(@_)          }
+    sub skip          { _skip(@_)          }
+    sub skip_rest     { _skip_rest(@_)     }
+    sub diag          { _diag(@_)          }
+    sub flunk         { _flunk(@_)         }
+    sub isa_ok        { _isa_ok(@_)        }
+    sub dies_ok       { _dies_ok(@_)       }
+    sub lives_ok      { _lives_ok(@_)      }
+    sub eval_dies_ok  { _eval_dies_ok(@_)  }
+    sub eval_lives_ok { _eval_lives_ok(@_) }
+    sub is_deeply     { _is_deeply(@_)     }
+    sub eq_array      { _is_deeply(@_)     }
+    sub done_testing  { _done_testing()    }
+    sub done          { _done()            }
 }
