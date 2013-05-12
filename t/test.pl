1eb2172b8146a319dda0df5543ff402709da83c1
Tobias Leich <email@froggs.de>
2013-05-12 16:13:36 +0200

    remove debug out

--- t/test.pl
+++ t/test.pl
@@ -7,7 +7,6 @@ sub _plan($plan) { plan($plan) }
     use v5;
     sub plan {
         my %args = scalar(@_) == 2 ? @_ : ('tests' => $_[0]);
-        say %args;
         _plan( $args{'tests'} )
     }
 }
