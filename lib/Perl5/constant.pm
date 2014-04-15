5c69e311f05e9e3f0d96fb921ef321420a99e0e8
Tobias Leich <email@froggs.de>
2014-04-15 22:39:48 +0200

    give constants their prototype
    So these subs do not take any arguments

--- lib/Perl5/constant.pm
+++ lib/Perl5/constant.pm
@@ -6,7 +6,9 @@ sub EXPORT(*@ops) {
     my %o;
 
     for @ops -> $k, $v {
-        %o{ '&' ~ $k } := sub { $v };
+        %o{ '&' ~ $k }  := -> { $v };
+        # constants do not swallow arguments
+        nqp::getlexrelcaller(nqp::ctx(), '%prototype'){$k} := '';
     }
 
     %o
